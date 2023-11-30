5225db88b2b4e929ad5988747fc62ae5fe4ba507
itiB <kazuyoshi.suzuki@zozo.com>
2023-12-01 08:52:17 +0900

    Updata router

--- lib/common/navigation/router.dart
+++ lib/common/navigation/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:memory_match_game/common/navigation/routes.dart';
+import 'package:memory_match_game/features/game_manager/ui/pages/memory_match_page.dart';
 import 'package:memory_match_game/features/game_manager/ui/pages/startup_page.dart';
 
 final router = GoRouter(
@@ -9,6 +10,14 @@ final router = GoRouter(
       path: '/',
       name: AppRoute.home.name,
       builder: (context, state) => const StartUpPage(),
+    ),
+    GoRoute(
+      path: '/game/:gameLevel',
+      name: AppRoute.game.name,
+      builder: (context, state) {
+        final gameLevel = int.parse(state.pathParameters['gameLevel']!);
+        return MemoryMatchPage(gameLevel: gameLevel);
+      },
     ),
   ],
   errorBuilder: (context, state) => Scaffold(
